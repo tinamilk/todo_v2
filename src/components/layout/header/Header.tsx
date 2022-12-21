31c5b19b3a5a0b26bc8ccdf1dfe81c6ea0bca4b3
Kristina <kristinamilkovic0@gmail.com>
2022-12-21 11:17:55 +0300

    Addsetting default filter

--- src/components/layout/header/Header.tsx
+++ src/components/layout/header/Header.tsx
@@ -2,16 +2,20 @@ import React from "react";
 import { AppHeading, HeaderWrapper, PageHeading } from "./header.styles";
 import { UserIcon } from "../../../assets/Icons";
 import { NavLink } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { setDefault } from "../../../redux/filters/filters";
 
 type HeaderProps = {
   pageHeading: string;
 };
 
 export const Header = ({ pageHeading }: HeaderProps) => {
+  const dispatch = useDispatch();
+
   return (
     <HeaderWrapper>
       <NavLink to="/" style={{ textDecoration: "none" }}>
-        <AppHeading>To-Do</AppHeading>
+        <AppHeading onClick={() => dispatch(setDefault())}>To-Do</AppHeading>
       </NavLink>
       <PageHeading>{pageHeading}</PageHeading>
       <NavLink to="/settings">
